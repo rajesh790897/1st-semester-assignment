3ec2daae9261df698acf544a5ec414b8ff8bd425
Rajesh Mondal <109855992+rajesh790897@users.noreply.github.com>
2024-11-17 11:35:15 +0530

    Create data_type_size.c

---
+++ data_type_size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Assignment No: 10");
+    printf("Size of char: %u\n", sizeof(char));
+    printf("Size of float: %u\n", sizeof(float));
+    printf("Size of int: %u\n", sizeof(int));
+    printf("Size of double: %u\n", sizeof(double));
+
+    return 0;
+}
