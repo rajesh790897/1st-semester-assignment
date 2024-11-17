505b1c7ab92c93a03b5959e9895726d1a831a5c8
Rajesh Mondal <109855992+rajesh790897@users.noreply.github.com>
2024-11-17 11:33:37 +0530

    Create ascii_value.c

---
+++ ascii_value.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("Assignment No: 9");
+    char character;
+
+    printf("Enter a character: ");
+    scanf("%c", &character);
+
+    printf("The ASCII value of '%c' is %d\n", character, character);
+
+    return 0;
+}
