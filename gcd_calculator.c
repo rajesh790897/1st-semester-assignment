a0413648d9cf4f69db86527ca4b6073ef61c1e83
Rajesh Mondal <109855992+rajesh790897@users.noreply.github.com>
2024-11-17 12:28:45 +0530

    Create gcd_calculator.c

---
+++ gcd_calculator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, res;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    while (num1 != num2) {
+        if (num1 > num2) {
+            num1 -= num2;
+        } else {
+            num2 -= num1;
+        }
+    }
+
+    res = num1;
+
+    printf("GCD of the two numbers is: %d\n", res);
+
+    return 0;
+}
