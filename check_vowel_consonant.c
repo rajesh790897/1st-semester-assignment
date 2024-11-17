c44e1d54bdb227440f9563737206acc1516134ac
Rajesh Mondal <109855992+rajesh790897@users.noreply.github.com>
2024-11-17 12:03:06 +0530

    Create check_vowel_consonant.c

---
+++ check_vowel_consonant.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char letter;
+
+    printf("Enter a letter: ");
+    scanf("%c", &letter);
+
+    switch (letter) {
+        case 'a':
+        case 'A':
+        case 'e':
+        case 'E':
+        case 'i':
+        case 'I':
+        case 'o':
+        case 'O':
+        case 'u':
+        case 'U':
+            printf("%c is a vowel.\n", letter);
+            break;
+        default:
+            printf("%c is a consonant.\n", letter);
+            break;
+    }
+
+    return 0;
+}
